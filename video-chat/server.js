07a73d7566869d82e65b3ab55e1c2794d3ef25a2
notedit <notedit@gmail.com>
2017-05-18 00:10:31 +0800

    add js sdk daemon

---
+++ video-chat/server.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+
+const app = express();
+
+const key = fs.readFileSync('keys/server.key');
+const cert = fs.readFileSync('keys/server.crt');
+
+
+app.use(express.static('public'));
+
+
+const server = https.createServer({
+    key: key,
+    cert: cert
+}, app);
+
+const port = 4000;
+
+server.listen(port,'localhost', function(){
+
+    console.log('listen on ' + port);
+    
+});
+
+
+
